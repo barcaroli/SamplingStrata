a91f82e090facb2c16e0e66d6bf367b1c18154bd
Giulio Barcaroli <gbarcaroli@gmail.com>
2018-05-20 18:03:16 +0200

    Add files via upload

---
+++ R/evalSolution.r
@@ -0,0 +1,176 @@
+evalSolution <- function (frame, outstrata, nsampl = 100, cens = NULL, 
+                          writeFiles = FALSE) 
+{
+  # if (writeFiles == TRUE) {
+  #   dire <- getwd()
+  #   direnew <- paste(dire,"/output",sep="")
+  #   if(!dir.exists(direnew)) dir.create(direnew)
+  #   setwd(direnew)
+  # }
+  numY <- length(grep("Y", toupper(colnames(frame))))
+  numdom <- length(levels(as.factor(frame$DOMAINVALUE)))
+  param <- array(0, c(numY, numdom))
+  for (i in 1:numY) {
+    stmt <- paste("param[i,] <- aggregate(frame$Y", i, ",by=list(frame$DOMAINVALUE),FUN=mean)[,2]", 
+                  sep = "")
+    eval(parse(text = stmt))
+  }
+  estim <- array(0, c(numdom, nsampl, numY))
+  differ <- array(0, c(numdom, nsampl, numY))
+  for (j in (1:nsampl)) {
+    samp <- selectSample(frame, outstrata, verbatim = FALSE)
+    if (!is.null(cens)) 
+      samp <- rbind(cens, samp)
+    for (k in 1:numY) {
+      stmt <- paste("estim[,j,k] <- aggregate(samp$Y", 
+                    k, "*samp$WEIGHT,by=list(samp$DOMAINVALUE),FUN=sum)[,2] / aggregate(samp$WEIGHT,by=list(samp$DOMAINVALUE),FUN=sum)[,2]", 
+                    sep = "")
+      eval(parse(text = stmt))
+    }
+  }
+  for (j in (1:nsampl)) {
+    for (i in (1:numdom)) {
+      for (k in 1:numY) {
+        differ[i, j, k] <- estim[i, j, k] - param[k, 
+                                                  i]
+      }
+    }
+  }
+  cv <- array(0, c(numdom, numY))
+#  bias <- array(0, c(numdom, numY))
+  for (k in 1:numY) {
+    for (i in (1:numdom)) {
+      cv[i, k] <- sd(estim[i, , k])/mean(estim[i, , k])
+#      bias[i, k] <- mean(differ[i, , k]/mean(estim[i, , k]))
+    }
+  }
+  cv <- as.data.frame(cv)
+  for (i in 1:numY) {
+    stmt <- paste("colnames(cv)[", i, "] <- c('CV", i, "')", 
+                  sep = "")
+    eval(parse(text = stmt))
+  }
+  cv$dom <- paste("DOM", c(1:numdom), sep = "")
+  if (writeFiles == TRUE) 
+    write.table(cv, "expected_cv.csv", sep = ",", row.names = FALSE, 
+                col.names = TRUE, quote = FALSE)
+  cv1 <- NULL
+  cv1$domainvalue <- rep((1:numdom), numY)
+  cv1$cv <- rep(0, (numY * numdom))
+  cv1$val <- rep(0, (numY * numdom))
+  cv1 <- as.data.frame(cv1)
+  for (k in (1:numY)) {
+    for (j in (1:numdom)) {
+      cv1$cv[(k - 1) * numdom + j] <- k
+      stmt <- paste("cv1$val[(k-1)*numdom + j] <- cv$CV", 
+                    k, "[j]", sep = "")
+      eval(parse(text = stmt))
+    }
+  }
+  # if (writeFiles == TRUE) 
+  #   write.table(cv1, "expected_cv1.csv", sep = ",", row.names = FALSE, 
+  #               col.names = TRUE, quote = FALSE)
+  # bias <- as.data.frame(bias)
+  # for (i in 1:numY) {
+  #   stmt <- paste("colnames(bias)[", i, "] <- c('RelBias", 
+  #                 i, "')", sep = "")
+  #   eval(parse(text = stmt))
+  # }
+  # bias$dom <- paste("DOM", c(1:numdom), sep = "")
+  # if (writeFiles == TRUE) 
+  #   write.table(bias, "expected_rel_bias.csv", sep = ",", 
+  #               row.names = FALSE, col.names = TRUE, quote = FALSE)
+  # bias1 <- NULL
+  # bias1$domainvalue <- rep((1:numdom), numY)
+  # bias1$bias <- rep(0, (numY * numdom))
+  # bias1$val <- rep(0, (numY * numdom))
+  # bias1 <- as.data.frame(bias1)
+  # for (k in (1:numY)) {
+  #   for (j in (1:numdom)) {
+  #     bias1$bias[(k - 1) * numdom + j] <- k
+  #     stmt <- paste("bias1$val[(k-1)*numdom + j] <- bias$RelBias", 
+  #                   k, "[j]", sep = "")
+  #     eval(parse(text = stmt))
+  #   }
+  # }
+  # if (writeFiles == TRUE) 
+  #   write.table(bias1, "expected_rel_bias1.csv", sep = ",", 
+  #               row.names = FALSE, col.names = TRUE, quote = FALSE)
+  diff <- NULL
+  diff$dom <- rep(0, numdom * nsampl)
+  diff$samp <- rep(0, numdom * nsampl)
+  for (i in (1:numY)) {
+    stmt <- paste("diff$diff", i, " <- rep(0,numdom*nsampl)", 
+                  sep = "")
+    eval(parse(text = stmt))
+  }
+  diff <- as.data.frame(diff)
+  for (i in (1:numdom)) {
+    for (j in (1:nsampl)) {
+      diff$dom[(i - 1) * nsampl + j] <- i
+      diff$samp[(i - 1) * nsampl + j] <- j
+      for (k in (1:numY)) {
+        stmt <- paste("diff$diff", k, "[(i-1)*nsampl+j] <- differ[i,j,k]", 
+                      sep = "")
+        eval(parse(text = stmt))
+      }
+    }
+  }
+  if (writeFiles == TRUE) 
+    write.table(diff, "differences.csv", sep = ",", row.names = FALSE, 
+                col.names = TRUE, quote = FALSE)
+  ############################################   
+# New code for bias  
+  bias <- NULL
+  for (i in (3:ncol(diff))) {
+    stmt <- paste("bias$y",i-2," <- tapply(diff$diff",i-2,",diff$dom,mean)",sep="")
+    eval(parse(text=stmt))
+  } 
+  bias$dom <- paste("DOM", c(1:numdom), sep = "")
+  bias <- as.data.frame(bias)
+  if (writeFiles == TRUE)
+    write.table(bias, "expected_rel_bias.csv", sep = ",",
+                row.names = FALSE, col.names = TRUE, quote = FALSE)
+############################################  
+  if (numdom > 1) {
+    if (writeFiles == TRUE) 
+      pdf("cv.pdf", width = 7, height = 5)
+    boxplot(val ~ cv, data = cv1, col = "orange", main = "Distribution of CV's in the domains", 
+            xlab = "Variables Y", ylab = "Value of CV")
+    if (writeFiles == TRUE) 
+      dev.off()
+    if (writeFiles == TRUE) 
+      pdf("rel_bias.pdf", width = 7, height = 5)
+    boxplot(bias[,-ncol(bias)], col = "orange", main = "Distribution of relative bias in the domains", 
+            xlab = "Variables Y", ylab = "Relative bias")
+    if (writeFiles == TRUE) 
+      dev.off()
+  }
+  if (writeFiles == TRUE) 
+    pdf("differences.pdf", width = 14, height = 10)
+  k <- ceiling(numY/4)
+  for (j in 1:k) {
+    split.screen(c(2, 2))
+    for (i in 1:4) {
+      if (i + 4 * (j - 1) <= numY) {
+        stmt <- paste("screen(", i, ")", sep = "")
+        eval(parse(text = stmt))
+        stmt <- paste("boxplot(diff", i, "~dom,data=diff,ylab='Differences',xlab='Domain',col = 'orange')", 
+                      sep = "")
+        eval(parse(text = stmt))
+        stmt <- paste("mtext(expression(Y", i, "), side=3, adj=0, cex=1.0, line=1)", 
+                      sep = "")
+        eval(parse(text = stmt))
+      }
+    }
+    close.screen(all.screens = TRUE)
+  }
+  if (writeFiles == TRUE) 
+    dev.off()
+  # results <- list(coeff_var = cv1, bias = bias1)
+  results <- list(coeff_var = cv, rel_bias = bias)
+  # if (writeFiles == TRUE) {
+  #   setwd(dire)
+  # }
+  return(results)
+}
---
+++ R/optimizeStrata.R
@@ -0,0 +1,181 @@
+optimizeStrata <- function (errors, 
+                            strata, 
+                            cens = NULL, 
+                            strcens = FALSE, 
+                            alldomains = TRUE, 
+                            dom = NULL, 
+                            initialStrata = NA, 
+                            addStrataFactor = 0.0, 
+                            minnumstr = 2, 
+                            iter = 50, 
+                            pops = 20, 
+                            mut_chance = NA, 
+                            elitism_rate = 0.2, 
+                            highvalue = 1e+08, 
+                            suggestions = NULL, 
+                            realAllocation = TRUE, 
+                            writeFiles = FALSE, 
+                            showPlot = TRUE) 
+{
+  if (writeFiles == TRUE) {
+    dire <- getwd()
+    direnew <- paste(dire,"/output",sep="")
+    if(!dir.exists(direnew)) dir.create(direnew)
+    setwd(direnew)
+  }
+  if (is.na(initialStrata)) initialStrata <- as.numeric(table(strata$DOM1))
+  nstrata = initialStrata
+	colnames(errors) <- toupper(colnames(errors))
+    colnames(strata) <- toupper(colnames(strata))
+    errors$DOMAINVALUE <- as.factor(errors$DOMAINVALUE)
+    erro <- split(errors, list(errors$DOMAINVALUE))
+    stcamp <- split(strata, list(strata$DOM1))
+    if (!is.null(suggestions)) suggestdom <- split(suggestions, list(suggestions$domainvalue))
+    if (strcens == TRUE) {
+		colnames(cens) <- toupper(colnames(cens))
+#       stcens <- split(cens, list(cens$DOM1))
+		k <- length(levels(as.factor(strata$DOM1)))
+		stcens <- NULL
+		for (i in (1:k)) {
+			stcens[[i]] <- cens[cens$DOM1 == i,]
+ 		}
+    }
+
+    ndom <- length(levels(as.factor(strata$DOM1)))
+    if (alldomains == TRUE) {
+      if (ndom > length(nstrata)) stop("'initialStrata' vector lenght (=",length(nstrata),") 
+is not compatible with number of domains (=",ndom,")
+Set initialStrata with a number of elements equal to the number of domains")
+        vettsol <- NULL
+        outstrata <- NULL
+        for (i in 1:ndom) {
+            cat("\n *** Domain : ", i, " ",as.character(errors$DOMAINVALUE[i]))
+            cat("\n Number of strata : ", nrow(stcamp[[i]]) )
+            erro[[i]] <- erro[[i]][, -ncol(errors)]
+            cens <- NULL
+            flagcens <- strcens
+            if (strcens == TRUE) {
+                if (nrow(stcens[[i]]) > 0) {
+                  cens <- stcens[[i]]
+                  flagcens = TRUE
+                }
+            }
+            if (strcens == TRUE) {
+                if (nrow(stcens[[i]]) == 0) {
+                  cens <- NULL
+                  flagcens = FALSE
+                }
+            }
+            if (strcens == FALSE) {
+                cens <- NULL
+                flagcens = FALSE
+            }
+            if (!is.null(suggestions)) {
+              suggest <- matrix(0,nrow=1,ncol=nrow(stcamp[[i]]))
+              suggest[1,] <- suggestdom[[i]]$suggestions
+            }
+            else {
+              suggest <- NULL
+            }
+            if (nrow(stcamp[[i]]) > 0) {
+                solut <- strataGenalg(errors = erro[[i]], 
+                  strata = stcamp[[i]], 
+                  cens = cens, 
+                  strcens = flagcens, 
+                  dominio = i, 
+                  initialStrata = nstrata[i], 
+                  minnumstr, 
+                  iter, 
+                  pops, 
+                  mut_chance, 
+                  elitism_rate, 
+                  addStrataFactor, 
+                  highvalue, 
+                  suggestions = suggest, 
+                  realAllocation, 
+                  writeFiles, 
+                  showPlot)
+				if (nrow(stcamp[[i]]) == 1) {
+	              solut <- list(c(1),stcamp[[i]][c(1:grep("DOM1",colnames(stcamp[[i]])))])
+                  error <- data.frame(erro[[i]])
+                  strat <- data.frame(solut[[2]])
+                  solut[[2]]$SOLUZ <- sum(bethel(strat, error, realAllocation = T))
+                  if (solut[[2]]$SOLUZ > solut[[2]]$N) solut[[2]]$SOLUZ <- solut[[2]]$N
+                }
+                vettsol <- c(vettsol, solut[[1]])
+				if (length(outstrata) > 0) colnames(outstrata) <- toupper(colnames(outstrata))
+				colnames(solut[[2]]) <- toupper(colnames(solut[[2]]))
+                outstrata <- rbind(outstrata, solut[[2]])
+            }
+ #           if (nrow(stcamp[[i]]) == 0 & flagcens == TRUE) {
+ #               sol <- NULL
+ #               t <- c(rep(1, nrow(stcens[[i]])))
+ #               sol <- aggrStrata(strata = stcens[[i]], nvar = (ncol(errors) - 
+ #                 2), vett = t, censiti = 1, dominio = i)
+ #               sol$soluz <- sol$N
+ #               vettsol <- c(vettsol, t)
+ #               outstrata <- rbind(outstrata, sol)
+ #           }
+        }
+    }
+    if (alldomains == FALSE) {
+        if (dom < 1 | dom > ndom) 
+            stop("\nInvalid value of the indicated domain\n")
+        i <- dom
+        erro[[i]] <- erro[[i]][, -ncol(errors)]
+		flagcens <- strcens
+		if (strcens == TRUE) {
+			flagcens <- TRUE
+			colnames(cens) <- toupper(colnames(cens))
+			censi <- cens[cens$DOM1 == i,]
+			if (nrow(censi) == 0) {
+				flagcens <- FALSE
+				censi <- NULL
+			}
+		}
+		if (!is.null(suggestions)) {
+		  suggest <- matrix(0,nrow=1,ncol=nrow(stcamp[[i]]))
+		  suggest[1,] <- suggestdom[[i]]$suggestions
+		}
+		else {
+		  suggest <- NULL
+		}		
+		solut <- strataGenalg(errors = erro[[i]], 
+								strata = stcamp[[i]], 
+								cens = censi, 
+								strcens = flagcens, 
+								dominio = i, 
+								initialStrata = nstrata, 
+								minnumstr, 
+								iter, 
+								pops, 
+								mut_chance, 
+								elitism_rate, 
+								addStrataFactor, 
+								highvalue, 
+								suggestions = suggest, 
+								realAllocation, 
+								writeFiles, 
+								showPlot)
+        vettsol <- solut[[1]]
+        outstrata <- solut[[2]]
+    }
+    colnames(outstrata) <- toupper(colnames(outstrata))
+    # new statement ---------------------------
+    # outstrata$SOLUZ <- round(outstrata$SOLUZ)
+    # -----------------------------------------
+    dimens <- sum(round(outstrata$SOLUZ))
+    cat("\n *** Sample size : ", dimens)
+    cat("\n *** Number of strata : ", nrow(outstrata))
+    cat("\n---------------------------")
+    if (writeFiles == TRUE) {
+        write.table(outstrata, file = "outstrata.txt", sep = "\t", 
+            row.names = FALSE, col.names = TRUE, quote = FALSE)
+        cat("\n...written output to outstrata.txt")
+    }
+    solution <- list(indices = vettsol, aggr_strata = outstrata)
+    if (writeFiles == TRUE) {
+      setwd(dire)
+    }
+    return(solution)
+}
