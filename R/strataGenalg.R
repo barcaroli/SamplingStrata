99cd4f0f9021823fca88c26af59bc503acb7dc86
Giulio Barcaroli <gbarcaroli@gmail.com>
2018-10-09 18:02:45 +0200

    Bug in strataGenalg

--- R/strataGenalg.R
+++ R/strataGenalg.R
@@ -84,8 +84,10 @@ strataGenalg <- function(errors, strata, cens, strcens,
         strcor <- aggrStrata(strata, nvar, floor(indices), censiti, 
             dominio)
         dimsamp <- nrow(strcor)
-        if (strcens == TRUE) 
+        # change ------------------------------------
+        if (strcens == TRUE)
             strcor <- rbind(strcor, cens)
+        #--------------------------------------------
         dimens <- nrow(strcor)
         # cat('\nCurrent solution:',indices) cat('\nNumber of input
         # strata:',dimens) cat('\n ...sampling strata:',dimsamp)
@@ -177,8 +179,15 @@ strataGenalg <- function(errors, strata, cens, strcens,
     censiti <- 0
     strcor <- aggrStrata(strata, nvar, v, censiti, dominio)
  #   if (strcens == TRUE) strcor <- rbind(strcor, cens)
-    soluz <- bethel(strcor, errors, minnumstr, printa = FALSE, 
-        realAllocation = realAllocation)
+#-----------------------------------------------------    
+# Here the change to take into account the censused strata
+    stratatot <- rbind(strcor,cens)
+    allstrata <- bethel(stratatot,errors,minnumstr,printa=T,
+                        realAllocation = realAllocation)
+    soluz <- as.numeric(attributes(allstrata)$confr[1:nrow(strcor),3])  
+#-----------------------------------------------------    
+    # soluz <- bethel(strcor, errors, minnumstr, printa = FALSE, 
+    #     realAllocation = realAllocation)
 	risulta <- cbind(strcor, soluz)
 	cat("\n *** Sample cost: ", sum(soluz))
 	cat(paste("\n *** Number of strata: ", nrow(strcor)))
