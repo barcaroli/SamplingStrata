1b1ed5be31f9856f091b7f61171fc9ae33a58a06
barcaroli <gbarcaroli@gmail.com>
2018-11-20 17:05:56 +0100

    strataGenalg2

--- R/strataGenalg2.R
+++ R/strataGenalg2.R
@@ -83,6 +83,7 @@ evaluate <- function(dataset,
 monitor <- function(obj) {
   if (showPlot == TRUE) {
     # ylim=c(min(obj$evaluation),max(obj$evaluation))
+    ylim=c(0,max(obj$evaluation))
     plot(obj$mean,type="l",col="red",ylim=ylim,xlab="Iterations",ylab="Sample size")
     points(obj$best,type="l",col="black")
   }
