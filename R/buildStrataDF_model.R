3c1b12a909374dc6279957f09bb8942c7346d4b1
barcaroli <gbarcaroli@gmail.com>
2018-11-26 20:31:03 -0500

    buildStrataDF

--- R/buildStrataDF_model.R
+++ R/buildStrataDF_model.R
@@ -20,7 +20,8 @@ buildStrataDF <- function(dataset,
         sqrt(sum(w * (x - mx)^2)/(sum(w)))
     }
     colnames(dataset) <- toupper(colnames(dataset))
-    levels(as.factor(dataset$DOMAINVALUE)) <- droplevels(as.factor(dataset$DOMAINVALUE))
+    dataset$DOMAINVALUE <- as.factor(dataset$DOMAINVALUE)
+    levels(dataset$DOMAINVALUE) <- droplevels(dataset$DOMAINVALUE)
     nvarX <- length(grep("X", names(dataset)))
     nvarY <- length(grep("Y", names(dataset)))
     if (length(grep("WEIGHT", names(dataset))) == 1) {
