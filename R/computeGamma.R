300b91c756ae79b29c3facfa13bac968e34c5cba
unknown <gbarcaroli@gmail.com>
2019-10-30 11:31:12 +0100

    last

--- R/computeGamma.R
+++ R/computeGamma.R
@@ -16,13 +16,13 @@ computeGamma <- function (e,
   std_eps <- sqrt(tapply(dataset$e,dataset$p_bins,var))
   boxplot(e ~ p_bins, data=dataset) 
   x_eps <- tapply(dataset$x,dataset$p_bins,mean)
-  plot(x_eps,std_eps)
-  plot(log(x_eps),log(std_eps))
+  # plot(x_eps,std_eps)
+  # plot(log(x_eps),log(std_eps))
   lm_gamma <- lm(log(std_eps)~log(x_eps))
   lm_gamma$coefficients
   gamma_est <- lm_gamma$coefficients[2]
   sigma <- exp(lm_gamma$coefficients[1])
-  gamma_est
+  # gamma_est
   ris_gamma[1] <- gamma_est
   ris_gamma[2] <- sigma
   return(ris_gamma)
