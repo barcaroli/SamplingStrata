994dc4c76b7a44d026913dbd9edd0508d9dda478
unknown <gbarcaroli@gmail.com>
2019-10-30 09:50:45 +0100

    help

--- R/computeGamma.R
+++ R/computeGamma.R
@@ -5,24 +5,26 @@
 # x : explanatory variable
 # nbins : number of bins in which categorize errors and explanatory variable
 
-computeGamma <- function (e,x,nbins) {
+computeGamma <- function (e,
+                          x,
+                          nbins=6) {
   dataset <- as.data.frame(list(e=e,x=x))
   colnames(dataset)
   require(SamplingStrata)
-  ris_gamma<-NULL
+  ris_gamma <- NULL
   dataset$p_bins <- var.bin(dataset$x,nbins)
   std_eps <- sqrt(tapply(dataset$e,dataset$p_bins,var))
   boxplot(e ~ p_bins, data=dataset) 
-  x_eps<-tapply(dataset$x,dataset$p_bins,mean)
+  x_eps <- tapply(dataset$x,dataset$p_bins,mean)
   plot(x_eps,std_eps)
   plot(log(x_eps),log(std_eps))
-  lm_gamma<-lm(log(std_eps)~log(x_eps))
+  lm_gamma <- lm(log(std_eps)~log(x_eps))
   lm_gamma$coefficients
-  gamma_est<-lm_gamma$coefficients[2]
-  sigma<-exp(lm_gamma$coefficients[1])
+  gamma_est <- lm_gamma$coefficients[2]
+  sigma <- exp(lm_gamma$coefficients[1])
   gamma_est
-  ris_gamma[1]<-gamma_est
-  ris_gamma[2]<-sigma
+  ris_gamma[1] <- gamma_est
+  ris_gamma[2] <- sigma
   return(ris_gamma)
 }
 
