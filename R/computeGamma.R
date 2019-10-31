d87eacdee0b34f5dfa2ee4a12248f46c07a32edc
unknown <gbarcaroli@gmail.com>
2019-10-31 09:06:09 +0100

    computeGamma

--- R/computeGamma.R
+++ R/computeGamma.R
@@ -1,30 +1,34 @@
-# Estimation of the coefficient of heteroscedasticity and variability 
+# Estimation of the coefficient of heteroscedasticity and residuals variability 
 
 # parameters
-# e : errors
+# e : residuals
 # x : explanatory variable
 # nbins : number of bins in which categorize errors and explanatory variable
 
 computeGamma <- function (e,
                           x,
-                          nbins=6) {
+                          nbins=6,
+                          showPlot = TRUE) {
   dataset <- as.data.frame(list(e=e,x=x))
   colnames(dataset)
   require(SamplingStrata)
   ris_gamma <- NULL
   dataset$p_bins <- var.bin(dataset$x,nbins)
   std_eps <- sqrt(tapply(dataset$e,dataset$p_bins,var))
-  boxplot(e ~ p_bins, data=dataset) 
+
   x_eps <- tapply(dataset$x,dataset$p_bins,mean)
-  # plot(x_eps,std_eps)
-  # plot(log(x_eps),log(std_eps))
-  lm_gamma <- lm(log(std_eps)~log(x_eps))
-  # lm_gamma$coefficients
-  gamma_est <- lm_gamma$coefficients[2]
-  sigma <- exp(lm_gamma$coefficients[1])
-  # gamma_est
-  ris_gamma[1] <- gamma_est
-  ris_gamma[2] <- sigma
+  lm_gamma <- lm(log(std_eps) ~ log(x_eps))
+  if (showPlot == TRUE) {
+    par(mfrow=c(1,2))
+    boxplot(e ~ p_bins, data = dataset, ylab="residuals", xlab="clusters",col="orange")
+    title("Distribution of residuals")
+    plot(log(x_eps), log(std_eps), xlab="log(mean(Y))", ylab="log(stdev(residuals))")
+    abline(lm_gamma)
+    title("Model")
+    par(mfrow=c(1,1))
+  }
+  ris_gamma[1] <- lm_gamma$coefficients[2]
+  ris_gamma[2] <- exp(lm_gamma$coefficients[1])
   return(ris_gamma)
 }
 
