9495d0c75393f8f97899a1abf51103252102879a
unknown <gbarcaroli@gmail.com>
2019-11-09 19:17:15 +0100

    computeGamma

--- R/computeGamma.R
+++ R/computeGamma.R
@@ -5,30 +5,31 @@
 # x : explanatory variable
 # nbins : number of bins in which categorize errors and explanatory variable
 
-computeGamma <- function (e,
-                          x,
-                          nbins=6,
-                          showPlot = TRUE) {
-  dataset <- as.data.frame(list(e=e,x=x))
+computeGamma <- function (e, x, nbins = 6, showPlot = TRUE) 
+{
+  dataset <- as.data.frame(list(e = e, x = x))
   colnames(dataset)
   require(SamplingStrata)
   ris_gamma <- NULL
-  dataset$p_bins <- var.bin(dataset$x,nbins)
-  std_eps <- sqrt(tapply(dataset$e,dataset$p_bins,var))
-
-  x_eps <- tapply(dataset$x,dataset$p_bins,mean)
+  dataset$p_bins <- var.bin(dataset$x, nbins)
+  std_eps <- sqrt(tapply(dataset$e, dataset$p_bins, var))
+  x_eps <- tapply(dataset$x, dataset$p_bins, mean)
   lm_gamma <- lm(log(std_eps) ~ log(x_eps))
+  summary(lm_gamma)$r.square
   if (showPlot == TRUE) {
-    par(mfrow=c(1,2))
-    boxplot(e ~ p_bins, data = dataset, ylab="residuals", xlab="clusters",col="orange")
+    par(mfrow = c(1, 2))
+    boxplot(e ~ p_bins, data = dataset, ylab = "residuals", 
+            xlab = "clusters", col = "orange")
     title("Distribution of residuals")
-    plot(log(x_eps), log(std_eps), xlab="log(mean(x))", ylab="log(stdev(residuals))")
+    plot(log(x_eps), log(std_eps), xlab = "log(mean(x))", 
+         ylab = "log(stdev(residuals))")
     abline(lm_gamma)
     title("Model")
-    par(mfrow=c(1,1))
+    par(mfrow = c(1, 1))
   }
   ris_gamma[1] <- lm_gamma$coefficients[2]
   ris_gamma[2] <- exp(lm_gamma$coefficients[1])
+  ris_gamma[3] <- summary(lm_gamma)$r.square
   return(ris_gamma)
 }
 
