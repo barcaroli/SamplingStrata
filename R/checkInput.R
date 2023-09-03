8babaeab6688ae658bbd232bf976d76d04fc5a07
barcaroli <gbarcaroli@gmail.com>
2023-09-03 17:22:02 +0200

    checkInput negative values

--- R/checkInput.R
+++ R/checkInput.R
@@ -56,7 +56,7 @@ checkInput <- function (errors = NULL, strata = NULL, sampframe = NULL)
         1) 
       stop("In frame dataframe the indication of the domain (DOMAINVALUE) is missing")
     for (i in (1:(sum(grepl("Y", colnames(sampframe)))))) {
-      stmt <- paste("if (min(sampframe$Y",i,") < 0) stop('Variable Y",i," has negative values in sampframe')",sep="")
+      stmt <- paste("if (min(sampframe$Y",i,") < 0) warning('Variable Y",i," has negative values in sampframe')",sep="")
       eval(parse(text=stmt))
     }
     for (i in (1:(sum(grepl("X", colnames(sampframe)))))) {
