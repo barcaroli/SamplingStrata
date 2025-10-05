32f8746847559004c7b6467e219befb28faea9e7
Giulio Barcaroli <gbarcaroli@gmail.com>
2025-10-05 23:50:27 +0200

    staging all files

---
+++ update_vignettes.R
@@ -0,0 +1,4 @@
+knitr::knit("vignettes/SamplingStrata.Rmd.orig", output = "vignettes/Sampling.Rmd")
+knitr::knit("vignettes/drake_continuous.Rmd.orig", output = "vignettes/drake_continuous.Rmd")
+knitr::knit("vignettes/models.Rmd.orig", output = "vignettes/models.Rmd")
+knitr::knit("vignettes/spatial.Rmd.orig", output = "vignettes/spatial.Rmd")
